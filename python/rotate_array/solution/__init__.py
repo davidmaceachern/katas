54d32ee2c03c60df25e878ffce66fad4c135f5b2
David MacEachern <david.maceachern.tech@gmail.com>
2021-03-09 09:43:19 +0100

    Add simpler approach

--- python/rotate_array/solution/__init__.py
+++ python/rotate_array/solution/__init__.py
@@ -51,5 +51,15 @@ class Solution(object):
             for j in range(len(nums)):
                 nums[j], previous = previous, nums[j]
 
-
-            
+# Simpler Approach
+# Time?
+# Space?
+# class Solution(object):
+#     def rotate(self, nums, k):
+#         """
+#         :type nums: List[int]
+#         :type k: int
+#         :rtype: None Do not return anything, modify nums in-place instead.
+#         """
+#         l = len(nums)
+#         nums[:] = nums[l-k:] + nums[:l-k]
